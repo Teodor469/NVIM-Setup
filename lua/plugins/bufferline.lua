9050a50b1afbda79d9506beaedcb2557a3dc59b6
Bebe Kote <lifetroll6@gmail.com>
2025-09-07 17:03:16 +0300

    added bufferline

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,29 @@
+return {
+  'akinsho/bufferline.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  version = "v4.*",
+  config = function()
+    require("bufferline").setup({
+      options = {
+        separator_style = "slant",
+        diagnostics = "nvim_lsp",
+        numbers = "buffer_id",
+        show_close_icon = true,
+        always_show_bufferline = false,
+        offsets = {
+          {
+            filetype = "NvimTree",
+            text = "File Explorer",
+            text_align = "left",
+            separator = true
+          }
+        }
+      }
+    })
+
+    vim.keymap.set('n', '<S-h>', '<cmd>BufferLineCyclePrev<cr>', { desc = 'Prev Buffer' })
+    vim.keymap.set('n', '<S-l>', '<cmd>BufferLineCycleNext<cr>', { desc = 'Next Buffer' })
+    vim.keymap.set('n', '<leader>c', '<cmd>bdelete<CR>', { desc = 'Close Buffer' })
+    vim.keymap.set('n', '<C-w>', '<cmd>bdelete<CR>', { desc = 'Close Buffer' })
+  end,
+}
