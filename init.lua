4a4340920ed1af775b760a917bc4fe296e20f877
Bebe Kote <lifetroll6@gmail.com>
2025-09-07 18:54:49 +0300

    tmux-nvim-nav

--- init.lua
+++ init.lua
@@ -3,6 +3,12 @@ vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.swapfile = false
+
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
 -- editor options
 vim.cmd("set expandtab")
